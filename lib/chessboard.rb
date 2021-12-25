433973375c11759e462b79f63996faf4b0b947d9
dawidl022 <domdaw7@gmail.com>
2021-12-25 14:12:15 +0000

    Fix pawn moving logic

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -3,7 +3,7 @@ require_relative '../boards'
 class Chessboard
   include Boards
 
-  attr_reader :board
+  attr_reader :board, :moves
 
   FILES = [:a, :b, :c, :d, :e, :f, :g, :h]
 
@@ -16,7 +16,7 @@ class Chessboard
       raise invalid_notation_error
     end
 
-    file = FILES.find_index { |letter| letter == notation[0].downcase.to_sym }
+    file = FILES.find_index(notation[0].downcase.to_sym)
     raise invalid_notation_error if file.nil?
 
     rank = notation[1].to_i - 1
@@ -43,14 +43,26 @@ class Chessboard
   end
 
   def move(notation, colour)
-    return move_pawn(notation, colour) if notation.length == 2
+    result = evaluate_move(notation, colour)
+
+    record_move(notation + ' e.p.', colour) if result == :ep
+    record_move(notation, colour) if result.equal?(true)
+
+    result
   end
 
   def has_moves?(colour)
   end
 
   def legal_moves(piece)
-    Set.new(piece.valid_moves.filter do |(move_rank, move_file)|
+    if piece.is_a?(Pawn)
+      # en passant logic
+      valid_moves = piece.valid_moves(@moves[-1] ? @moves[-1][-1] : nil)
+    else
+      valid_moves = piece.valid_moves
+    end
+
+    Set.new(valid_moves.filter do |(move_rank, move_file)|
       cloned_chessboard = Chessboard.new
       cloned_board = clone_board
 
@@ -80,15 +92,35 @@ class Chessboard
 
   private
 
+  def evaluate_move(notation, colour)
+    return promote_pawn(notation, colour) if pawn_promotion_notation?(notation)
+    return move_pawn(notation, colour) if pawn_notation?(notation)
+  end
+
+  def record_move(notation, colour)
+    if colour == :white
+      @moves << [notation]
+    else
+      @moves[-1] << notation
+    end
+  end
+
   def move_pawn(notation, colour)
     begin
-      target = self.class.notation_to_indices(notation)
+      target = self.class.notation_to_indices(notation[-2, 2])
     rescue ArgumentError => error
       return error.message
     end
 
     rank = target[0]
-    file = target[1]
+
+    if notation.length == 4
+      file = FILES.find_index(notation[0].to_sym)
+      # TODO extract error messages into constants
+      return 'Invalid move' unless file
+    else
+      file = target[1]
+    end
 
     diff = colour == :white ? -1 : 1
 
@@ -102,15 +134,43 @@ class Chessboard
       end
 
       if legal_moves(piece).include?(target)
-        @board[target[0]][file] = piece
+        en_passant = notation[1] == 'x' && @board[target[0]][target[1]].nil?
+
+        @board[target[0]][target[1]] = piece
         @board[rank - diff][file] = nil
-        return true
+
+        return en_passant ? :ep : true
       else
         return 'Illegal move'
       end
     end
 
     return 'Invalid move'
+  end
+
+  def promote_pawn(notation, colour)
+    target = self.class.notation_to_indices(notation[0, 2])
+    rank, file = target
+
+    pawn = @board[rank - 1][file]
+
+    return 'Illegal move' unless legal_moves(pawn).include?(target)
+    if notation.length == 2
+      return "Specify a piece to promote to: #{notation}Q, #{notation}R, " \
+        "#{notation}B or #{notation}N"
+    end
+
+    case notation[2]
+    when 'Q' then @board[rank][file] = Queen.new(colour, @board)
+    when 'R' then @board[rank][file] = Rook.new(colour, @board)
+    when 'B' then @board[rank][file] = Bishop.new(colour, @board)
+    when 'N' then @board[rank][file] = Knight.new(colour, @board)
+    else return 'Invalid promotion piece'
+    end
+
+    @board[rank - 1][file] = nil
+
+    true
   end
 
   def find_piece(piece, colour)
@@ -156,5 +216,20 @@ class Chessboard
         end
       end
     end
+
+    clone
+  end
+
+  def pawn_notation?(notation)
+    notation.length == 2 \
+    || (lowercase?(notation[0]) && notation[1] == 'x' && notation.length == 4)
+  end
+
+  def pawn_promotion_notation?(notation)
+    notation =~ /[a-h]8[QRBN]?/
+  end
+
+  def lowercase?(string)
+    string == string.downcase
   end
 end
--- spec/chessboard_spec.rb
+++ spec/chessboard_spec.rb
@@ -83,6 +83,8 @@ RSpec.describe Chessboard do
         pawn = board.board[1][4]
         board.move('e4', :white)
         expect(pawn.position).to eq([3, 4])
+        expect(board.board[3][4]).to be pawn
+        expect(board.board[1][4]).to be_nil
       end
 
       it 'moves another white pawn forward by providing chess notation' do
@@ -93,12 +95,14 @@ RSpec.describe Chessboard do
 
       it 'moves a black pawn forward by providing chess notation' do
         pawn = board.board[6][4]
+        board.moves << []
         board.move('e5', :black)
         expect(pawn.position).to eq([4, 4])
       end
 
       it 'moves another black pawn forward by providing chess notation' do
         pawn = board.board[6][4]
+        board.moves << []
         board.move('e5', :black)
         expect(pawn.position).to eq([4, 4])
       end
@@ -149,6 +153,150 @@ RSpec.describe Chessboard do
           expect(response).to eq('Illegal move')
         end
       end
+
+      context 'capture' do
+        it 'pawn captures another pawn' do
+          white_pawn = board.board[1][4]
+          board.move('e4', :white)
+          board.move('d5', :black)
+          board.move('exd5', :white)
+          expect(white_pawn.position).to eq([4, 3])
+          expect(board.board[4][3]).to be white_pawn
+          expect(board.board[3][4]).to be_nil
+        end
+      end
+
+      context 'promotion' do
+        context 'when given promoting piece' do
+          before do
+            board.instance_variable_set(:@board, empty_board)
+            board.board[5][2] = King.new(:black, board.board, [7, 4])
+            board.board[5][5] = King.new(:white, board.board, [0, 4])
+            board.board[6][4] = Pawn.new(:white, board.board, [1, 3])
+          end
+
+          it 'removes the pawn from the board' do
+            pawn = board.board[6][4]
+            board.move('e8Q', :white)
+            expect(pawn.position).to be_nil
+          end
+
+          context 'when Q is noted' do
+            it 'place a white queen on the board' do
+              pawn = board.board[6][4]
+              board.move('e8Q', :white)
+              expect(board.board[7][4]).to be_a(Queen)
+              expect(board.board[7][4].colour).to eq(:white)
+            end
+          end
+
+          context 'when R is noted' do
+            it 'place a rook on the board' do
+              pawn = board.board[6][4]
+              board.move('e8R', :white)
+              expect(board.board[7][4]).to be_a(Rook)
+            end
+          end
+
+          context 'when B is noted' do
+            it 'place a bishop on the board' do
+              pawn = board.board[6][4]
+              board.move('e8B', :white)
+              expect(board.board[7][4]).to be_a(Bishop)
+            end
+          end
+
+          context 'when N is noted' do
+            it 'place a knight on the board' do
+              pawn = board.board[6][4]
+              board.move('e8N', :white)
+              expect(board.board[7][4]).to be_a(Knight)
+            end
+          end
+
+          context 'when given an invalid piece to promote to' do
+            it 'returns a message' do
+              pawn = board.board[6][4]
+              expect(board.move('e8K', :white)).to eq('Invalid promotion piece')
+            end
+
+            it 'does not move the pawn' do
+              pawn = board.board[6][4]
+              expect { board.move('e8K', :white) }.to_not change { board.board }
+              expect { board.move('e8K', :white) }
+                .to_not change { pawn.position }
+            end
+          end
+
+        end
+        context 'when not given promoting piece' do
+          before do
+            board.instance_variable_set(:@board, empty_board)
+            board.board[5][2] = King.new(:black, board.board, [7, 4])
+            board.board[5][5] = King.new(:white, board.board, [0, 4])
+            board.board[6][4] = Pawn.new(:white, board.board, [1, 3])
+          end
+
+          it 'returns a message giving possible options' do
+            pawn = board.board[6][4]
+            expect(board.move('e8', :white)).to eq(
+              'Specify a piece to promote to: e8Q, e8R, e8B or e8N'
+            )
+          end
+        end
+      end
+    end
+
+    describe 'saves moves to move history matrix' do
+      describe 'sequence of pawn moves and captures' do
+        it 'is recorded as entered' do
+          expect do
+            board.move('e4', :white)
+            board.move('d5', :black)
+            board.move('exd5', :white)
+          end.to change { board.moves }.from([]).to([['e4', 'd5'], ['exd5']])
+        end
+      end
+
+      context 'en passant' do
+        it 'is recorded with e.p.' do
+          expect do
+            board.move('e4', :white)
+            board.move('c5', :black)
+            board.move('e5', :white)
+            board.move('d5', :black)
+            board.move('exd6', :white)
+          end.to change { board.moves }.from([]).to([
+            ['e4', 'c5'], ['e5', 'd5'], ['exd6 e.p.']
+          ])
+        end
+      end
+    end
+
+    describe "Knight" do
+      context 'when it is unambiguous' do
+        context 'on a starting board as white' do
+          it 'moves Knight to c3' do
+            pending 'pawn not finished'
+            knight = board.board[0][1]
+            board.move("Nc3", :white)
+            expect(knight.position).to eq([2, 2])
+            expect(board.board[2][2]).to be knight
+            expect(board.board[0][1]).to be_nil
+          end
+        end
+
+
+        context 'on a starting board as black'
+      end
+
+      context 'when additional notation is supplied to disambiguate'
+
+      context 'when it is ambiguous'
+
+      context 'when move causes player to be under check'
+
+      context 'invalid'
     end
   end
 
@@ -265,7 +413,7 @@ RSpec.describe Chessboard do
 
     it 'does not mutate the chessboard' do
       king = board.board[6][4]
-      expect{ board.legal_moves(king) }.to_not change { board }
+      expect{ board.legal_moves(king) }.to_not change { board.board }
     end
   end
 end
